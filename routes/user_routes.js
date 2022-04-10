935673cadd57ae7aa9366a2e551c52c9452e0ec5
OsvaldoBarragan <barraganosvaldo39@gmail.com>
2022-04-10 02:09:30 -0400

    Added sign-in route

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
-// const crypto = require('crypto')
+const crypto = require('crypto')
 const bcrypt = require('bcrypt')
 const bcryptSaltRounds = 10
 
 const errors = require('./../lib/custom_errors')
 const BadParamsError = errors.BadParamsError
+const BadCredentialsError = errors.BadCredentialsError
 const User = require('./../models/user')
+
+// const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
@@ -38,6 +41,35 @@ router.post('/sign-up', (req, res, next) => {
     })
     .then(user => User.create(user))
     .then(user => res.status(201).json({ user: user.toObject() }))
+    .catch(next)
+})
+
+router.post('/sign-in', (req, res, next) => {
+  const pw = req.body.credentials.password
+  let user
+
+  User.findOne({ username: req.body.credentials.username })
+    .then(record => {
+      if (!record) {
+        throw new BadCredentialsError()
+      }
+      user = record
+
+      return bcrypt.compare(pw, user.hashedPassword)
+    })
+    .then(correctPassword => {
+      if (correctPassword) {
+        const token = crypto.randomBytes(16).toString('hex')
+        user.token = token
+
+        return user.save()
+      } else {
+        throw new BadCredentialsError()
+      }
+    })
+    .then(user => {
+      res.status(201).json({ user: user.toObject() })
+    })
     .catch(next)
 })
 
