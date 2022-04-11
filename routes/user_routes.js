5bfbe7998fec7f0fffceb9810e921ebcea7a54dd
OsvaldoBarragan <barraganosvaldo39@gmail.com>
2022-04-11 12:28:06 -0400

    .

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const crypto = require('crypto')
-const bcrypt = require('bcrypt')
-const bcryptSaltRounds = 10
+// const crypto = require('crypto')
+// const bcrypt = require('bcrypt')
+// const bcryptSaltRounds = 10
 
 const errors = require('./../lib/custom_errors')
-const BadParamsError = errors.BadParamsError
-const BadCredentialsError = errors.BadCredentialsError
+// const BadParamsError = errors.BadParamsError
+// const BadCredentialsError = errors.BadCredentialsError
 const User = require('./../models/user')
 
 // const requireToken = passport.authenticate('bearer', { session: false })
@@ -22,55 +22,55 @@ router.get('/users', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/sign-up', (req, res, next) => {
-  Promise.resolve(req.body.credentials)
-    .then(credentials => {
-      if (!credentials ||
-        !credentials.password ||
-        credentials.password !== credentials.password_confirmation) {
-          throw new BadParamsError()
-        }
-    })
-    .then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
-    .then(hash => {
-      return {
-        email: req.body.credentials.email,
-        username: req.body.credentials.username,
-        hashedPassword: hash
-      }
-    })
-    .then(user => User.create(user))
-    .then(user => res.status(201).json({ user: user.toObject() }))
-    .catch(next)
-})
-
-router.post('/sign-in', (req, res, next) => {
-  const pw = req.body.credentials.password
-  let user
-
-  User.findOne({ username: req.body.credentials.username })
-    .then(record => {
-      if (!record) {
-        throw new BadCredentialsError()
-      }
-      user = record
-
-      return bcrypt.compare(pw, user.hashedPassword)
-    })
-    .then(correctPassword => {
-      if (correctPassword) {
-        const token = crypto.randomBytes(16).toString('hex')
-        user.token = token
-
-        return user.save()
-      } else {
-        throw new BadCredentialsError()
-      }
-    })
-    .then(user => {
-      res.status(201).json({ user: user.toObject() })
-    })
-    .catch(next)
-})
+// router.post('/sign-up', (req, res, next) => {
+//   Promise.resolve(req.body.credentials)
+//     .then(credentials => {
+//       if (!credentials ||
+//         !credentials.password ||
+//         credentials.password !== credentials.password_confirmation) {
+//           throw new BadParamsError()
+//         }
+//     })
+//     .then(() => bcrypt.hash(req.body.credentials.password, bcryptSaltRounds))
+//     .then(hash => {
+//       return {
+//         email: req.body.credentials.email,
+//         username: req.body.credentials.username,
+//         hashedPassword: hash
+//       }
+//     })
+//     .then(user => User.create(user))
+//     .then(user => res.status(201).json({ user: user.toObject() }))
+//     .catch(next)
+// })
+//
+// router.post('/sign-in', (req, res, next) => {
+//   const pw = req.body.credentials.password
+//   let user
+//
+//   User.findOne({ username: req.body.credentials.username })
+//     .then(record => {
+//       if (!record) {
+//         throw new BadCredentialsError()
+//       }
+//       user = record
+//
+//       return bcrypt.compare(pw, user.hashedPassword)
+//     })
+//     .then(correctPassword => {
+//       if (correctPassword) {
+//         const token = crypto.randomBytes(16).toString('hex')
+//         user.token = token
+//
+//         return user.save()
+//       } else {
+//         throw new BadCredentialsError()
+//       }
+//     })
+//     .then(user => {
+//       res.status(201).json({ user: user.toObject() })
+//     })
+//     .catch(next)
+// })
 
 module.exports = router
