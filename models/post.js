7d6787b9fcb89982bd630424d21fd2a115c0af7b
OsvaldoBarragan <barraganosvaldo39@gmail.com>
2022-04-13 00:35:52 -0400

    Quick Save

--- models/post.js
+++ models/post.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-  
+  imgSource: {
+    type: String,
+    required: true
+  },
+  caption: {
+    type: String,
+    required: true
+  },
+  hashtags: [String],
+  likes: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
 })
+
+module.exports = mongoose.model('Post', postSchema)
